ca1d2fa6780f8414ea5788f08323db4890bdd23c
Dev Kumar <y2ksoft@gmail.com>
2017-11-15 13:45:47 +1300

    import firebase libraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,12 @@ import { ItemsListComponent } from './items/items-list/items-list.component';
 import { ItemsDetailComponent } from './items/items-detail/items-detail.component';
 import { ItemsFormComponent } from './items/items-form/items-form.component';
 
+// Firebase Import
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+
+
 @NgModule({
   declarations: [
     AppComponent,
